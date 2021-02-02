1a724e851cd9b6a12199eda93abed7fbe58dc043
LZdziechowski <l.zdziechowski.contact@gmail.com>
2021-02-02 01:12:08 +0100

    configure project

---
+++ more-patterns/src/test/java/com/patterns2/TestApplication.java
@@ -0,0 +1,12 @@
+package com.patterns2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestApplication {
+
+    @Test
+    void contextLoads() {
+    }
+}
---
+++ src/test/java/com/TestApplication.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TestApplication {
+
+
+    @Test
+    public void contextLoad() {
+
+
+    }
+}
